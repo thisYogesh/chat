fee173c3061d557d978974f706987c48f06938fc
Yogesh Jagdale <you@example.com>
2020-09-13 21:03:25 +0530

    added web socket

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const http = require('http')
-http.createServer(function(req, res){
-    res.end('Hello Yogesh!')
-}).listen(process.env.PORT || 3000)
+const server = http.createServer(function(req, res){
+    res.setHeader('content-type', 'text/html')
+    res.end(`
+        <h1>Hello Yogesh!</h1>
+        <div class='time'></div>
+
+        <script>
+            var HOST = location.origin.replace(/^http/, 'ws')
+            var ws = new WebSocket(HOST);
+            var el;
+
+            ws.onmessage = function (event) {
+            el = document.querySelector('.time')
+            el.innerHTML = event.data;
+        };
+        </script>
+    `)
+});
+
+server.listen(process.env.PORT || 3000)
+
+const { Server } = require('ws');
+
+const wss = new Server({ server });
+wss.on('connection', (ws) => {
+    console.log('WS Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+    wss.clients.forEach((client) => {
+      client.send(new Date().toTimeString());
+    });
+}, 1000);
