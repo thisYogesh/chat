0e1bf1be661a2c75f0b1d92a9cd252c71a7fa423
Yogesh <yog99mail@gmail.com>
2020-10-06 15:48:44 +0530

    created api for adding user

---
+++ app.js
@@ -0,0 +1,24 @@
+const request = require('requester')
+const db = require('./db')
+
+request.post('/create-user', function(req, res, { payload }){
+    res.setHeader('Content-Type', 'application/json')
+    
+    db
+    .create('users', JSON.parse(payload))
+    .then(resp => {
+        console.log('User added!')
+        res.end(JSON.stringify({
+            done: true
+        }))
+    })
+    .catch(error => {
+        console.log('Unable to add user!')
+        res.end(JSON.stringify({
+            done: false
+        }))
+    })
+})
+
+db.init()
+request.port(8080)
---
+++ db.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'yogesh',
+    database: 'chatbase'
+})
+
+const db = {
+    init: function() {
+        connection.connect(function(error){
+            if(error){
+                console.log('Error in database connection', error);
+                return;
+            }
+        
+            console.log('Database is connected successfully!')
+        })
+    },
+
+    create: function(table, config){
+        const fields = Object.keys(config)
+        const values = Object
+            .values(config)
+            .reduce(function(values, value){
+            if(values) values += ',';
+            
+            if(typeof value === 'string') values += `'${value}'`
+            else values += value
+
+            return values
+        }, '')
+
+        const sql = `insert into ${table}(${fields.join(',')}) values(${values})`
+        
+        return new Promise(function(resolve, reject){
+            connection.query(sql, function(error){
+                if(error){
+                    console.log('Error in executing sql query', error)
+                    reject(false);
+                    return;
+                }else{
+                    resolve(true);
+                    return;
+                }
+            })
+        })
+    },
+
+    update: function(){},
+
+    delete: function(){}
+}
+
+module.exports = db
