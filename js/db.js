73c3f04d84cff3da899723055305add703d77ac4
Yogesh Jagdale <yog99mail@gmail.com>
2020-11-21 22:05:01 +0530

    add pre-commit linting

--- js/db.js
+++ js/db.js
@@ -1,55 +1,53 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'yogesh',
-    database: 'chatbase'
-})
+  host: 'localhost',
+  user: 'root',
+  password: 'yogesh@001',
+  database: 'chatbase',
+});
 
 const db = {
-    init: function() {
-        connection.connect(function(error){
-            if(error){
-                console.log('Error in database connection', error);
-                return;
-            }
-        
-            console.log('Database is connected successfully!')
-        })
-    },
+  init: function () {
+    connection.connect(function (error) {
+      if (error) {
+        console.log('Error in database connection', error);
+        return;
+      }
 
-    create: function(table, config){
-        const fields = Object.keys(config)
-        const values = Object
-            .values(config)
-            .reduce(function(values, value){
-            if(values) values += ',';
-            
-            if(typeof value === 'string') values += `'${value}'`
-            else values += value
+      console.log('Database is connected successfully!');
+    });
+  },
 
-            return values
-        }, '')
+  create: function (table, config) {
+    const fields = Object.keys(config);
+    const values = Object.values(config).reduce(function (values, value) {
+      if (values) values += ',';
 
-        const sql = `insert into ${table}(${fields.join(',')}) values(${values})`
-        
-        return new Promise(function(resolve, reject){
-            connection.query(sql, function(error){
-                if(error){
-                    console.log('Error in executing sql query', error)
-                    reject(false);
-                    return;
-                }else{
-                    resolve(true);
-                    return;
-                }
-            })
-        })
-    },
+      if (typeof value === 'string') values += `'${value}'`;
+      else values += value;
 
-    update: function(){},
+      return values;
+    }, '');
 
-    delete: function(){}
-}
+    const sql = `insert into ${table}(${fields.join(',')}) values(${values})`;
 
-module.exports = db
+    return new Promise(function (resolve, reject) {
+      connection.query(sql, function (error) {
+        if (error) {
+          console.log('Error in executing sql query', error);
+          reject(false);
+          return;
+        } else {
+          resolve(true);
+          return;
+        }
+      });
+    });
+  },
+
+  update: function () {},
+
+  delete: function () {},
+};
+
+module.exports = db;
